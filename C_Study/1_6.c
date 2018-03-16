0491aa939c6cbd707a60ed6c32c91b148f57b9ba
liguoxiong <liguoxiong-it@bestpay.com.cn>
2018-03-16 17:31:56 +0800

    算法

--- C_Study/1_6.c
+++ C_Study/1_6.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-int main() {
-	int max(int a, int b);
-	int c, d, e;
-	scanf_s("%d,%d,%d", &c, &d, &e);
-	if (c > max(d, e))
-		printf("最大值是：%d\n", c);
-	else
-		printf("最大值是：%d\n", max(d, e));
-}
+//int main() {
+//	int max(int a, int b);
+//	int c, d, e;
+//	scanf_s("%d,%d,%d", &c, &d, &e);
+//	if (c > max(d, e))
+//		printf("最大值是：%d\n", c);
+//	else
+//		printf("最大值是：%d\n", max(d, e));
+//}
 
 int max(int a, int b) {
 	if (a > b)
