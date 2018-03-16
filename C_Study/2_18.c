47fb91cdd384bfd29e9d83481c6fa7c2f43700d8
liguoxiong <liguoxiong-it@bestpay.com.cn>
2018-03-16 17:38:20 +0800

    5!

--- C_Study/2_18.c
+++ C_Study/2_18.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-int main() {
-	int i, j;
-	i = 1;
-	j = 1;
-	while (j<=5)
-	{
-		i = i * j;
-		j = j + 1;
-	}
-	printf("%d\n", i);
-	return 0;
-
-}
+//int main() {
+//	int i, j;
+//	i = 1;
+//	j = 1;
+//	while (j<=5)
+//	{
+//		i = i * j;
+//		j = j + 1;
+//	}
+//	printf("%d\n", i);
+//	return 0;
+//
+//}
