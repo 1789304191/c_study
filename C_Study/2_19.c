a037e501567ecf39a0c752e399e6207f0e2f684e
liguoxiong <liguoxiong-it@bestpay.com.cn>
2018-03-16 18:28:55 +0800

    计算1-1/2.....

---
+++ C_Study/2_19.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+//int main() {
+//	int a = 1;
+//	double b = 1.0;
+//	double c = 1.0;
+//	while (c<100)
+//	{
+//		a = a * (-1);
+//		c = c + 1;
+//		b = b + a/c;
+//	}
+//	printf("%f\n", b);
+//	return 0;
+//}
+
+
+int main() {
+	int sign = 1;
+	double deno = 2.0, sum = 1.0, term;
+	while (deno<=100)
+	{
+		sign = -sign;
+		term = sign / deno;
+		sum = sum + term;
+		deno = deno + 1;
+	}
+	printf("%f\n",sum);
+	return 0;
+}
