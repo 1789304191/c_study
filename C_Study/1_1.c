f99fa2166b498f07eef52c0a1df22d230f1e5bc1
liguoxiong <liguoxiong-it@bestpay.com.cn>
2018-03-16 13:24:22 +0800

    习题1.6

--- C_Study/1_1.c
+++ C_Study/1_1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-int main() {
-	printf("***************************\n");
-	printf("Very good !\n");
-	printf("***************************\n");
-	return 0;
-}
+//int main() {
+//	printf("***************************\n");
+//	printf("very good !\n");
+//	printf("***************************\n");
+//	return 0;
+//}
---
+++ C_Study/1_6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+	int max(int a, int b);
+	int c, d, e;
+	scanf_s("%d,%d,%d", &c, &d, &e);
+	if (c > max(d, e))
+		printf("最大值是：%d\n", c);
+	else
+		printf("最大值是：%d\n", max(d, e));
+}
+
+int max(int a, int b) {
+	if (a > b)
+		return a;
+	else
+		return b;
+}
