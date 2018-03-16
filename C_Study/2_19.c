ce52e2cee772b6e05bd41907f56d1b09d314a177
1789304191 <1789304191@qq.com>
2018-03-17 01:11:17 +0800

    注释main方法

--- C_Study/2_19.c
+++ C_Study/2_19.c
@@ -14,16 +14,16 @@
 //}
 
 
-int main() {
-	int sign = 1;
-	double deno = 2.0, sum = 1.0, term;
-	while (deno<=100)
-	{
-		sign = -sign;
-		term = sign / deno;
-		sum = sum + term;
-		deno = deno + 1;
-	}
-	printf("%f\n",sum);
-	return 0;
-}
+//int main() {
+//	int sign = 1;
+//	double deno = 2.0, sum = 1.0, term;
+//	while (deno<=100)
+//	{
+//		sign = -sign;
+//		term = sign / deno;
+//		sum = sum + term;
+//		deno = deno + 1;
+//	}
+//	printf("%f\n",sum);
+//	return 0;
+//}
