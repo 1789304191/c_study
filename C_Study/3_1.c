1bc9d5d7d18cad5d4b8c812aecd8ec9b3b91ed8f
liguoxiong <1789304191@qq.com>
2018-03-19 00:01:05 +0800

    例题310

---
+++ C_Study/3_1.c
@@ -0,0 +1,30 @@
+#include <math.h>
+#include <stdio.h>
+
+//int main() {
+//	float p, r, n;
+//	r = 0.1;
+//	n = 10;
+//	p = pow(1+r,10);
+//	printf("p=%f\n", p);
+//	return 0;
+//	//在编译时利率会按双精度处理，在像float单精度赋值时可能会损失精度，所以会出现警告
+//	//如果想去掉警告可以定义为double,float只能保证6位有效数字
+//}
+
+//int main() {
+//	float d = 300000, p = 6000, r = 0.01, m;
+//	m = lg10(p / (p - d * r)) / lg10(1 + r);
+//	printf("m=%f\n", m);
+//	return 0;
+//}
+
+//int main() {
+//	//printf("size=%d", sizeof(int));
+//	//return 0;
+//	//unsigned char c = 128;
+//	char c = 128;
+//	printf("%d:%c\n", c, c);
+//	//输出是-128，在存储单元中是用整数的补码存放，128的补码是他的二进制10000000，
+//	//转换成10进制，是-128
+//}
