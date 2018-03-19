fa46c6669e4ebb072491bc0b1ea90ddfa326eae2
liguoxiong <1789304191@qq.com>
2018-03-20 00:13:01 +0800

    第三章习题

--- C_Study/3_1.c
+++ C_Study/3_1.c
@@ -14,8 +14,8 @@
 
 //int main() {
 //	float d = 300000, p = 6000, r = 0.01, m;
-//	m = lg10(p / (p - d * r)) / lg10(1 + r);
-//	printf("m=%f\n", m);
+//	m = log(p / (p - d * r)) / log(1 + r);
+//	printf("m=%6.1f\n", m);
 //	return 0;
 //}
 
---
+++ C_Study/work_4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//int main() {
+//	char c1, c2;
+//	/*c1 = 97;
+//	c2 = 98;*/
+//	c1 = 197;
+//	c2 = 198;
+//	printf("c1=%c,c2=%c", c1, c2);
+//	printf("c1=%d,c2=%d", c1, c2);
+//	return 0;
+//	//http://blog.sina.com.cn/s/blog_70ec9a6f01014j1h.html
+//}
