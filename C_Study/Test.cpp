3c9f5d18d73320dc64e024bdf8a1a93504f230af
liguoxiong <1789304191@qq.com>
2018-03-20 23:47:30 +0800

    第四章，选择结构程序

---
+++ C_Study/Test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+	float score;
+	scanf_s("%f", &score);
+	bool a, b;
+	a = score >= 60;
+	b = score <= 69;
+	if (a == true && b == true) {
+		printf("the grade is C\n");
+	}
+	putchar('\a');
+	return 0;
+}
