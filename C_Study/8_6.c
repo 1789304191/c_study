5372cf656091374321fd4f33c4f44043ff8c7e2d
liguoxiong <liguoxiong-it@bestpay.com.cn>
2018-03-30 09:17:52 +0800

    注释

---
+++ C_Study/8_6.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+//int main() {
+//	void testOne();
+//	void testTwo();
+//	testTwo();
+//}
+//
+//void testOne() {
+//	int a[10];
+//	int i;
+//	printf("enter 10 integer numbers:");
+//	for (i = 0; i < 10; i++) {
+//		scanf_s("%d", &a[i]);
+//	}
+//	for (i = 0; i < 10; i++) {
+//		printf("%d ", a[i]);
+//	}
+//	printf("\n");
+//	return 0;
+//}
+//
+//void testTwo() {
+//	int a[10];
+//	int i;
+//	printf("enter 10 integer numbers:");
+//	for (i = 0; i < 10; i++) {
+//		scanf_s("%d", &a[i]);
+//	}
+//	for (i = 0; i < 10; i++) {
+//		printf("%d ", * (a+i));
+//	}
+//	printf("\n");
+//	return 0;
+//}
