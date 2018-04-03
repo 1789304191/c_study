88065aaee118f9bfb5645518fbbbd33289e5fe9a
liguoxiong <1789304191@qq.com>
2018-04-04 00:24:22 +0800

    动态内存分配与指向他的指针变量

---
+++ C_Study/8_31.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int *p, i;
+	void check(int * p);
+	//分配五个Int空间返回首地址
+	p =(int *)malloc(5 * sizeof(int));
+	for (i = 0; i < 5; i++) {
+		//通过p指向所输入的数
+		scanf_s("%d",p+i);
+	}
+	check(p);
+}
+
+void check(int * p) {
+	for (int  i = 0; i < 5; i++) {
+		//数组下标法
+		if (p[i]> 60) {
+			printf("%d",p[i]);
+		}
+	}
+
+}
