a1ba244b7046cf0d451dc4d0b3f7690094d83922
liguoxiong <1789304191@qq.com>
2018-04-11 03:12:46 +0800

    链表正向和反向排序； 两个链表相连之后排序;

--- C_Study/9_10.c
+++ C_Study/9_10.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include "Student.h"
 
-extern struct Student * del(struct Student * p);
-extern struct Student * insert(struct Student * head, struct Student * add);
-extern struct Student * create();
-void print_(struct Student * p);
-
+/*
+	假如p=1,2,3,4,5,6;
+		p1=p.next;
+		p1=2,3,4,5,6;
+		p.next=null;
+		p=1,null;
+		p1=2,3,4,5,6
+*/
 
 int main() {
-	struct Student * sortlinkedList(struct Student * head);
-	struct Student * insertionSort(struct Student * head);
-	struct Student * p=create();
-	print_(insertionSort(p));
+	void print_(struct Student * p);
+	struct Student * create();
+	struct Student * insertionSortByDesc(struct Student * head);
+	struct Student * insertionSortByAsc(struct Student * head);
+	struct Student * connect(struct Student * p1, struct Student * p2);
+	printf("\n请输入第一个链表:\n");
+	struct Student * p1 = create();
+	printf("\n请输入第二个链表:\n");
+	struct Student * p2 = create();
+	print_(insertionSortByAsc(connect(p1,p2)));
 	return 0;
 }
 
@@ -50,24 +59,77 @@ struct Student *sortlinkedList(struct Student * head)
 	return head;
 }
 
-//链表插入排序
-struct Student * insertionSort(struct Student * head) {
-	struct Student * p;
+//链表插入排序（从大到小）
+struct Student * insertionSortByDesc(struct Student * head) {
+	struct Student * p0 = NULL;
 	struct Student * p1;
 	struct Student * p2;
-	p1 = p = head;
-	p2 = p->next;
-	while (p->next != NULL) {
-		p2 = p;
-		while (p2->score < p1->score&&p2->next!=NULL) {
-			
+	struct Student * t;
+	p1 = head;
+	t = p2 = head->next;
+	head->next = NULL;
+	while (t!= NULL) {
+		while (p1 != NULL&&p2->score < p1->score) {
+			p0 = p1;
+			p1 = p1->next;
 		}
+		t = t->next;
 		if (p1 == head) {
 			head = p2;
+			p2->next = p1;
 		}
-		p2->next = p1;
+		else {
+			p0->next = p2;
+			p2->next = NULL;
+		}
+		p1 = head;
+		p2 = t;
 	}
 	return head;
 }
 
+
+//链表插入排序（从小到大）
+struct Student * insertionSortByAsc(struct Student * head) {
+	struct Student * p;
+	struct Student * p0 = NULL;
+	struct Student * p1;
+	struct Student * p2;
+	p1 = head;
+	p2 = head->next;
+	p = head->next;
+	head->next = NULL;
+	while (p != NULL) {
+		p = p->next;
+		while (p1 != NULL && p2->score > p1->score) {
+			p0 = p1;
+			p1 = p1->next;
+		}
+		//说明p2比p1小
+		if (p1 == head) {
+			head = p2;
+			p2->next = p1;
+		}
+		else {
+			p0->next = p2;
+			p2->next = NULL;
+		}
+		p1 = head;
+		p2 = p;
+	}
+	return head;
+}
+
+//两个链表连接
+struct Student * connect(struct Student * p1, struct Student * p2)
+{
+	struct Student * head = p1;
+	struct Student * p0 = NULL;
+	do {
+		p0 = p1;
+		p1 = p1->next;
+	} while (p1 != NULL);
+	p0->next = p2;
+	return head;
+};
 
